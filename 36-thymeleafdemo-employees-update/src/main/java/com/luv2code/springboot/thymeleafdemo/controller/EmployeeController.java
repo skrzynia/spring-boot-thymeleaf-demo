c5268149ebf306b106c18761aeca85334bbc542b
Wojtek <skrzynia777@gmail.com>
2019-10-24 10:59:06 +0100

    Add files via upload

---
+++ 36-thymeleafdemo-employees-update/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,71 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+	
+		
+	@Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
+
+	@GetMapping("/list")
+	public String listEmployees(Model model) {
+			
+		List<Employee> theEmployees = employeeService.findAll();
+		
+		model.addAttribute("employees", theEmployees);
+		
+		return "employees/list-employees";
+	}
+	
+	@GetMapping("/showAddForm")
+	public String showAddForm(Model model) {
+		
+		Employee employee = new Employee();
+		
+		model.addAttribute("employee", employee);
+		
+		return "employees/employee-form";
+	}
+	
+	
+	@GetMapping("/showUpdateForm")
+	public String showUpdateForm(@RequestParam("employeeId") int theId , Model model) {
+		
+		Employee theEmployee = employeeService.findById(theId);
+		
+		model.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		employeeService.save(theEmployee);
+		
+		return "redirect:/employees/list";
+	}
+	
+	
+	
+}
